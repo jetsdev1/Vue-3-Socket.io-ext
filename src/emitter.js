60bbe941b774ad63b134e17acf9b8202c6024282
jetsdev1 <126627024+jetsdev1@users.noreply.github.com>
2023-02-28 23:33:17 +0000

    Update emitter.js

--- src/emitter.js
+++ src/emitter.js
@@ -1,136 +1,144 @@
-import Logger from './logger';
+import CryptoJS from "crypto-js";
+import Logger from "./logger";
 
-export default class EventEmitter{
+export default class EventEmitter {
+  constructor(vuex = {}, secret) {
+    Logger.info(vuex ? `Vuex adapter enabled` : `Vuex adapter disabled`);
+    Logger.info(
+      vuex.mutationPrefix
+        ? `Vuex socket mutations enabled`
+        : `Vuex socket mutations disabled`
+    );
+    Logger.info(
+      vuex ? `Vuex socket actions enabled` : `Vuex socket actions disabled`
+    );
+    this.store = vuex.store;
+    this.actionPrefix = vuex.actionPrefix ? vuex.actionPrefix : "SOCKET_";
+    this.mutationPrefix = vuex.mutationPrefix;
+    this.listeners = new Map();
+    this.secret = secret;
+  }
 
-    constructor(vuex = {}){
-        Logger.info(vuex ? `Vuex adapter enabled` : `Vuex adapter disabled`);
-        Logger.info(vuex.mutationPrefix ? `Vuex socket mutations enabled` : `Vuex socket mutations disabled`);
-        Logger.info(vuex ? `Vuex socket actions enabled` : `Vuex socket actions disabled`);
-        this.store = vuex.store;
-        this.actionPrefix = vuex.actionPrefix ? vuex.actionPrefix : 'SOCKET_';
-        this.mutationPrefix = vuex.mutationPrefix;
-        this.listeners = new Map();
+  /**
+   * register new event listener with vuejs component instance
+   * @param event
+   * @param callback
+   * @param component
+   */
+  addListener(event, callback, component) {
+    if (typeof callback === "function") {
+      if (!this.listeners.has(event)) this.listeners.set(event, []);
+      this.listeners.get(event).push({ callback, component });
+
+      Logger.info(`#${event} subscribe, component: ${component.$options.name}`);
+    } else {
+      throw new Error(`callback must be a function`);
     }
+  }
 
-    /**
-     * register new event listener with vuejs component instance
-     * @param event
-     * @param callback
-     * @param component
-     */
-    addListener(event, callback, component){
+  /**
+   * remove a listenler
+   * @param event
+   * @param component
+   */
+  removeListener(event, component) {
+    if (this.listeners.has(event)) {
+      const listeners = this.listeners
+        .get(event)
+        .filter((listener) => listener.component !== component);
 
-        if(typeof callback === 'function'){
+      if (listeners.length > 0) {
+        this.listeners.set(event, listeners);
+      } else {
+        this.listeners.delete(event);
+      }
 
-            if (!this.listeners.has(event)) this.listeners.set(event, []);
-            this.listeners.get(event).push({ callback, component });
+      Logger.info(
+        `#${event} unsubscribe, component: ${component.$options.name}`
+      );
+    }
+  }
 
-            Logger.info(`#${event} subscribe, component: ${component.$options.name}`);
-
-        } else {
-
-            throw new Error(`callback must be a function`);
-
+  /**
+   * broadcast incoming event to components
+   * @param event
+   * @param args
+   */
+  emit(event, args) {
+    if (this.listeners.has(event)) {
+      if (this.secret !== undefined) {
+        if (
+          args !== undefined &&
+          typeof args === "object" &&
+          Object.hasOwn(args, "msg")
+        ) {
+          args = this._decrypt(args.msg);
         }
 
+        Logger.info(`Encrypted Broadcasting: #${event}, Data:`, args);
+      } else {
+        Logger.info(`Broadcasting: #${event}, Data:`, args);
+      }
+
+      this.listeners.get(event).forEach((listener) => {
+        listener.callback.call(listener.component, args);
+      });
     }
 
-    /**
-     * remove a listenler
-     * @param event
-     * @param component
-     */
-    removeListener(event, component){
-
-        if(this.listeners.has(event)){
-
-            const listeners = this.listeners.get(event).filter(listener => (
-                listener.component !== component
-            ));
-
-            if (listeners.length > 0) {
-                this.listeners.set(event, listeners);
-            } else {
-                this.listeners.delete(event);
-            }
-
-            Logger.info(`#${event} unsubscribe, component: ${component.$options.name}`);
-
-        }
-
+    if (event !== "ping" && event !== "pong") {
+      this.dispatchStore(event, args);
     }
+  }
 
-    /**
-     * broadcast incoming event to components
-     * @param event
-     * @param args
-     */
-    emit(event, args){
+  /**
+   * dispatching vuex actions
+   * @param event
+   * @param args
+   */
+  dispatchStore(event, args) {
+    if (this.store && this.store._actions) {
+      let prefixed_event = this.actionPrefix + event;
 
-        if(this.listeners.has(event)){
+      for (let key in this.store._actions) {
+        let action = key.split("/").pop();
 
-            Logger.info(`Broadcasting: #${event}, Data:`, args);
+        if (action === prefixed_event) {
+          Logger.info(`Dispatching Action: ${key}, Data:`, args);
 
-            this.listeners.get(event).forEach((listener) => {
-                listener.callback.call(listener.component, args);
-            });
-
+          this.store.dispatch(key, args);
         }
+      }
 
-        if(event !== 'ping' && event !== 'pong') {
-            this.dispatchStore(event, args);
+      if (this.mutationPrefix) {
+        let prefixed_event = this.mutationPrefix + event;
+
+        for (let key in this.store._mutations) {
+          let mutation = key.split("/").pop();
+
+          if (mutation === prefixed_event) {
+            Logger.info(`Commiting Mutation: ${key}, Data:`, args);
+
+            this.store.commit(key, args);
+          }
         }
-
+      }
     }
+  }
 
-
-    /**
-     * dispatching vuex actions
-     * @param event
-     * @param args
-     */
-    dispatchStore(event, args){
-
-        if(this.store && this.store._actions){
-
-            let prefixed_event = this.actionPrefix + event;
-
-            for (let key in this.store._actions) {
-
-                let action = key.split('/').pop();
-
-                if(action === prefixed_event) {
-
-                    Logger.info(`Dispatching Action: ${key}, Data:`, args);
-
-                    this.store.dispatch(key, args);
-
-                }
-
-            }
-
-            if(this.mutationPrefix) {
-
-                let prefixed_event = this.mutationPrefix + event;
-
-                for (let key in this.store._mutations) {
-
-                    let mutation = key.split('/').pop();
-
-                    if(mutation === prefixed_event) {
-
-                        Logger.info(`Commiting Mutation: ${key}, Data:`, args);
-
-                        this.store.commit(key, args);
-
-                    }
-
-                }
-
-            }
-
-        }
-
+  _decrypt(encrypted) {
+    try {
+      const decrypted = encrypted.map((item) =>
+        JSON.parse(
+          CryptoJS.AES.decrypt(item, this.secret).toString(CryptoJS.enc.Utf8)
+        )
+      );
+      return decrypted[0];
+    } catch (e) {
+      const error = new Error(
+        `Couldn't decrypt. Wrong secret used on client or invalid data sent. (${e.message})`
+      );
+      error.code = "ERR_DECRYPTION_ERROR";
+      throw error;
     }
-
+  }
 }
